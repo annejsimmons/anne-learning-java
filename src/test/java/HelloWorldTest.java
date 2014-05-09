1753b027d462ad277a0b4f4adc080733293361c3
Anne Simmons <annejsimmons@gmail.com>
2014-05-09 15:29:28 +0100

    Basic gradle file with a hello world class and junit test

---
+++ src/test/java/HelloWorldTest.java
@@ -0,0 +1,17 @@
+
+
+import org.hamcrest.core.Is;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+/**
+* Created by annejsimmons on 5/6/14.
+*/
+public class HelloWorldTest {
+    @Test
+    public void shouldSayHello() {
+        assertThat(true).isTrue();
+    }
+}
