d076773a1721c7b5ad81c5586419132dec2be3e2
Anne Simmons <annejsimmons@gmail.com>
2014-05-14 09:10:03 +0100

    Basic hello world drop wizard app

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -5,10 +5,6 @@ import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-
-/**
-* Created by annejsimmons on 5/6/14.
-*/
 public class HelloWorldTest {
     @Test
     public void shouldSayHello() {
